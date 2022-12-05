ca31b7441c98fc43a595060fc231ae144a5d684f
AndrewBonds55 <92964384+AndrewBonds55@users.noreply.github.com>
2022-12-04 21:10:56 -0600

    Adding Files

--- MOWERS/django_project/settings.py
+++ MOWERS/django_project/settings.py
@@ -131,6 +131,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]  # new
+STATICFILES_DIRS = [BASE_DIR / "static"]  # new
+STATIC_ROOT = BASE_DIR / "staticfiles"  # new
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"  # new
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
